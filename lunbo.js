c88b847a27ef2aa8cc36587e39a781921225c024
ranxu <ranxu07_18@163.com>
2015-09-29 20:42:22 +0800

    第二版，引入jquery,加上可以设置图片宽高的设置，如果只设置一个属性，则另外一个自适应，如果两个都没加，则有一个默认宽高，另外，LunBo类第一个参数为字符串类型的窗器id，第二个参数才是可选配置参数

--- lunbo.js
+++ lunbo.js
@@ -1,26 +1,56 @@
 (function(window,undefined){
 
 	//多长时间执行一次，左右按钮是否显示，小图标是否显示
-	function LunBo(opt){
+	function LunBo(container_id,opt){
+		if(typeof container_id!='string'){
+			console.log('第一个参数请输入一个字符串的容器id');
+			return;
+		}
+		this.can_play = false;//初始化完后才能开始动画
+		this.container_id = container_id;//轮播图窗口id
 		this.opts = opt || {};
 		this.timer = null;//定时器句柄
-		this.time = this.opts.time || 500;//轮播切换时间
+		this.time = this.opts.time || 1500;//轮播切换时间
 		this.complete = this.opts.complete || null;//每次播完后的回调函数
-		this.container_id = this.opts.container_id || 'lunboId';//轮播图窗口id
 		this.arrow_btn_show = this.opts.arrow_btn_show || true;//默认显示左右两个按钮
 		this.dots_show = this.opts.dots_show || true;//默认显示小图标（将来可以考虑小图标的显示位置，左，右，中）
-
+		this.img_width = this.opts.img_width;//默认图片的宽度
+		this.img_height = this.opts.img_height;//默认图片的高度
+		this.$imgs = $('#'+this.container_id).find('li img');//轮播的图片
+		this.init();
 	}
 	LunBo.prototype = {
 		constructor : LunBo,
+		init : function(){
+			this.set_img_wh();//设置图片的宽高
+			this.can_play = true;
+		},
 		play : function(){
 			var _this = this;
-			this.timer = setInterval(function(){
-				_this.xiaoguo();
-			},this.time);
+			if(this.can_play){
+				this.timer = setInterval(function(){
+					_this.xiaoguo();
+				},this.time);
+			}
 		},
 		xiaoguo : function(){
 			console.log('轮播效果展示');
+		},
+		//设置图片的宽高,如果用户只设置了其中一种，则另外一个是auto
+		set_img_wh : function(){
+			if(this.img_width && this.img_height){//都设置了
+				console.log('宽高都设置了');
+				this.$imgs.css({width:this.img_width,height:this.img_height});
+			}else if(this.img_width){
+				console.log('宽设置了');
+				this.$imgs.css({width:this.img_width});
+			}else if(this.img_height){
+				console.log('高设置了');
+				this.$imgs.css({height:this.img_height});
+			}else{
+				console.log('没设置宽高时的默认宽高');
+				this.$imgs.css({width:200,height:100});
+			}
 		}
 	}
 
